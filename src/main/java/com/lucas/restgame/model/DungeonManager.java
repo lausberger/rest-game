0b18240cbc9b32b29156786179617b2be5a6d6aa
Lucas Ausberger <ausbergerl@hotmail.com>
2023-09-13 19:38:40 -0500

    Add placeholder DungeonManager interface methods

--- src/main/java/com/lucas/restgame/model/DungeonManager.java
+++ src/main/java/com/lucas/restgame/model/DungeonManager.java
@@ -1,6 +1,7 @@
 package com.lucas.restgame.model;
 
 import com.lucas.restgame.entity.Dungeon;
+import com.lucas.restgame.entity.DungeonInstance;
 
 public class DungeonManager {
 
@@ -8,5 +9,29 @@ public class DungeonManager {
 
     public DungeonManager(Dungeon dungeon) {
         this.dungeon = dungeon;
+    }
+
+    public void handleEnter(String playerID) {
+
+    }
+
+    public void handleExit(String playerID) {
+
+    }
+
+    public void handleNextRoom(String playerID) {
+
+    }
+
+    public void handlePreviousRoom(String playerID) {
+
+    }
+
+    public void handleInitiateCombat(String playerID) {
+
+    }
+
+    public DungeonInstance getDungeonInstance(String playerID) {
+        return dungeon.getDungeonInstance(playerID);
     }
 }
